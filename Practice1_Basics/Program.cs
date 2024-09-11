4843a25917ec493d715a5680bf37d0ae0a4a3b09
akshit.saxena <akshit.saxena@DEL1-LHP-N72301>
2024-09-12 00:44:32 +0530

    Add project files.

---
+++ Practice1_Basics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice1_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int productId = 101;
+            string productName = "Laptop";
+            double productPrice = 65000;
+            int quantity = 200;
+
+            //display details
+            Console.WriteLine("Product ID: " + productId);
+            Console.WriteLine("Product Name: " + productName);
+            Console.WriteLine("Product Price " + productPrice);
+            Console.WriteLine("Quantity Avaialble " + quantity);
+
+            if(quantity > ) {
+
+        }
+    }
+}
